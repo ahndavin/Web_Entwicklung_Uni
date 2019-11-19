22fdf7d67b7b71b972b2c284e932c34901e421ee
jonashoen <hoenisch.jonas@gmail.com>
2019-11-19 13:17:33 +0100

    added attributes in festival class

--- src/main/java/festivalmanager/festival/Festival.java
+++ src/main/java/festivalmanager/festival/Festival.java
@@ -1,4 +1,24 @@
 package festivalmanager.festival;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Festival {
+	private @Id @GeneratedValue long id;
+	private final int maxVisitors;
+	private int currentVisitors;
+
+	private boolean sellingTickets;
+
+	private final List<String> plan = new ArrayList<>();
+
+	public Festival(int maxVisitors, boolean sellingTickets) {
+		this.maxVisitors = maxVisitors;
+
+		this.sellingTickets = sellingTickets;
+	}
 }
