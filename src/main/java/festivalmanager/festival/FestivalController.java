c67a41983433c54377e8e7931348f4de6b363b4a
jonashoen <hoenisch.jonas@gmail.com>
2019-11-22 21:08:59 +0100

    added functionality to add new festival

--- src/main/java/festivalmanager/festival/FestivalController.java
+++ src/main/java/festivalmanager/festival/FestivalController.java
@@ -3,6 +3,9 @@ package festivalmanager.festival;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FestivalController {
@@ -17,5 +20,20 @@ public class FestivalController {
 		model.addAttribute("festivals", festivals.findAll());
 
 		return "festivals";
+	}
+
+	@GetMapping("/festival/add")
+	String addFestival(Model model) {
+		model.addAttribute("festival_form", new FestivalForm());
+
+		return "add_festival";
+	}
+
+	@PostMapping("/festival/add")
+	String addFestival(@ModelAttribute FestivalForm festivalForm) {
+
+		festivals.save(festivalForm.toFestival());
+
+		return "redirect:/festivals";
 	}
 }
