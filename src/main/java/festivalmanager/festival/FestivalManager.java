2ac97baa94ca23b8805c19f809a7b0520f678b41
jonashoen <hoenisch.jonas@gmail.com>
2019-11-22 19:55:22 +0100

    added FestivalManager class

---
+++ src/main/java/festivalmanager/festival/FestivalManager.java
@@ -0,0 +1,32 @@
+package festivalmanager.festival;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FestivalManager {
+	private final FestivalRepository festivalRepository;
+
+	public FestivalManager(FestivalRepository festivalRepository) {
+		this.festivalRepository = festivalRepository;
+	}
+
+	public Iterable<Festival> findAll() {
+		return festivalRepository.findAll();
+	}
+
+	public boolean save(Festival festival) {
+		Iterable<Festival> festivals = festivalRepository.findAll();
+
+		for(Festival f : festivals) {
+			if(f.getLocation().equals(festival.getLocation())) {
+				if(Festival.areAtTheSameTime(festival, f)) {
+					return false;
+				}
+			}
+		}
+
+		festivalRepository.save(festival);
+
+		return true;
+	}
+}
