dfd39cf1a83626285a0c3082be5bbfbb1d388355
jonashoen <hoenisch.jonas@gmail.com>
2019-11-22 20:03:37 +0100

    adapted FestivalController to use FestivalManager

--- src/main/java/festivalmanager/festival/FestivalController.java
+++ src/main/java/festivalmanager/festival/FestivalController.java
@@ -1,16 +1,14 @@
 package festivalmanager.festival;
 
-import org.salespointframework.catalog.Catalog;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FestivalController {
-	private FestivalRepository festivals;
+	private FestivalManager festivals;
 
-	public FestivalController(FestivalRepository festivals) {
+	public FestivalController(FestivalManager festivals) {
 		this.festivals = festivals;
 	}
 
