7b2a9fff916fe540e4d6e54920560e86a16e780f
jonashoen <hoenisch.jonas@gmail.com>
2019-11-23 17:50:10 +0100

    added funtionality to edit festivals and some comments

--- src/main/java/festivalmanager/festival/FestivalController.java
+++ src/main/java/festivalmanager/festival/FestivalController.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 import java.util.Arrays;
+import java.util.Optional;
 
 @Controller
 public class FestivalController {
@@ -17,6 +18,9 @@ public class FestivalController {
 		this.festivals = festivals;
 	}
 
+	/**
+	 *	list all existing festivals
+	 */
 	@GetMapping("/festivals")
 	String festivals(Model model) {
 		model.addAttribute("festivals", festivals.findAll());
@@ -24,45 +28,98 @@ public class FestivalController {
 		return "festivals";
 	}
 
+	/**
+	 *
+	 * @param festivalName: name of the festival, passed in the URL
+	 * @param festivalId:	id of the festival, passed in the URL
+	 */
 	@GetMapping("/festival/{festivalName}-{festivalId}")
 	String festival(@PathVariable String festivalName, @PathVariable long festivalId, Model model) {
-		Festival festival = null;
+		Optional<Festival> festival = festivals.findById(festivalId);
 
-		for(Festival f : festivals.findAll()) {
-			if(f.getName().equals(festivalName) && f.getId() == festivalId) {
-				festival = f;
-				break;
-			}
-		}
-
-		if(festival == null) {
+		if(festival.isEmpty()) {
 			return "redirect:/404";
 		}
 
-		model.addAttribute("festival", festival);
+		model.addAttribute("festival", festival.get());
 
 		return "festival_detail";
 	}
 
+
+	/**
+	 * add a new festival
+	 */
 	@GetMapping("/festival/add")
 	String addFestival(Model model) {
 		model.addAttribute("festival_form", new FestivalForm());
 
-		return "add_festival";
+		return "festival_add";
 	}
 
+	/**
+	 *
+	 * @param festivalForm: form the user filed with information about the new festival
+	 * @param errors: errors from validation of festival data
+	 */
 	@PostMapping("/festival/add")
 	String addFestival(@ModelAttribute @Valid FestivalForm festivalForm, Errors errors, RedirectAttributes redirectAttributes) {
-		boolean result = false;
-
-		if(!errors.hasErrors()) {
-			result = festivals.save(festivalForm.toFestival());
-		}
+		boolean result = saveOrUpdate(festivalForm, errors);
 
 		if(!result) {
 			redirectAttributes.addFlashAttribute("error", "Fehler beim erstellen des Festivals.");
 		}
 
 		return "redirect:/festivals";
+	}
+
+	/**
+	 *
+	 * @param festivalId: id of the festival
+	 */
+	@GetMapping("/festival/{festivalName}-{festivalId}/edit")
+	String editFestival(@PathVariable long festivalId, Model model) {
+		Optional<Festival> festival = festivals.findById(festivalId);;
+
+		if(festival.isEmpty()) {
+			return "redirect:/404";
+		}
+
+		model.addAttribute("festival_form", festival.get());
+
+		System.out.println(festival.get().toString());
+
+		return "festival_edit";
+	}
+
+	/**
+	 *
+	 * @param festivalForm: form with the new data
+	 * @param errors: errors from validation of the festival data
+	 */
+	@PostMapping("/festival/edit")
+	String editFestival(@ModelAttribute @Valid FestivalForm festivalForm, Errors errors, RedirectAttributes redirectAttributes) {
+		boolean result = saveOrUpdate(festivalForm, errors);
+
+		if(!result) {
+			redirectAttributes.addFlashAttribute("error", "Fehler beim editieren des Festivals.");
+		}
+
+		return "redirect:/festival/" + festivalForm.getName() + "-" + festivalForm.getId();
+	}
+
+	/**
+	 * helper method to reduce duplicate code
+	 */
+	private boolean saveOrUpdate(FestivalForm festivalForm, Errors errors) {
+		boolean result = false;
+
+		Festival festival = festivalForm.toFestival();
+
+		if(!errors.hasErrors()) {
+			result = festivals.save(festival);
+		}
+
+		return result;
 	}
 }
