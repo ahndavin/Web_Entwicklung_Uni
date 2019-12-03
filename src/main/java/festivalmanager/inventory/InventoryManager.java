29706a80e08337dca0b369a274fc6dc966a5f187
jonashoen <hoenisch.jonas@gmail.com>
2019-12-03 13:51:31 +0100

    implemented basic methods for InventoryManager

--- src/main/java/festivalmanager/inventory/InventoryManager.java
+++ src/main/java/festivalmanager/inventory/InventoryManager.java
@@ -1,4 +1,26 @@
 package festivalmanager.inventory;
 
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
 public class InventoryManager {
+	private InventoryRepository inventory;
+
+	public InventoryManager(InventoryRepository inventory) {
+		this.inventory = inventory;
+	}
+
+	public void save(Item item) {
+		inventory.save(item);
+	}
+
+	public Iterable<Item> findAll() {
+		return inventory.findAll();
+	}
+
+	public Optional<Item> findById(Long id) {
+		return inventory.findById(id);
+	}
 }
