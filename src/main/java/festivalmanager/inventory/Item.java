db91cfd058d301b89e5eb9b855d9d3b1eb7ed679
jonashoen <hoenisch.jonas@gmail.com>
2019-12-03 13:39:34 +0100

    added constructor and attributes

--- src/main/java/festivalmanager/inventory/Item.java
+++ src/main/java/festivalmanager/inventory/Item.java
@@ -1,4 +1,41 @@
 package festivalmanager.inventory;
 
-public class Item {
+import org.javamoney.moneta.Money;
+import org.salespointframework.catalog.Product;
+import org.salespointframework.quantity.Metric;
+import org.salespointframework.quantity.Quantity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Item extends Product {
+	private Money cost;
+	private Quantity minimalQuantity;
+
+	public Item(String name, Money price, Money cost, Quantity minimalQuantity, String[] categories) {
+		super(name, price, Metric.UNIT);
+
+		this.cost = cost;
+		this.minimalQuantity = minimalQuantity;
+
+		for(String category : categories) {
+			addCategory(category);
+		}
+	}
+
+	public Money getCost() {
+		return cost;
+	}
+
+	public void setCost(Money cost) {
+		this.cost = cost;
+	}
+
+	public Quantity getMinimalQuantity() {
+		return minimalQuantity;
+	}
+
+	public void setMinimalQuantity(Quantity minimalQuantity) {
+		this.minimalQuantity = minimalQuantity;
+	}
 }
