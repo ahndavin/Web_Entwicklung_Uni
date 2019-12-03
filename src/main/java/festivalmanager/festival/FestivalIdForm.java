daca04f940e51cd62313ad69efd4cd7839d5f5e1
Kurdey <tobias.neubauer@mailbox.tu-dresden.de>
2019-12-03 17:29:00 +0100

    Addapted the FestivalInitializer

--- src/main/java/festivalmanager/festival/FestivalIdForm.java
+++ src/main/java/festivalmanager/festival/FestivalIdForm.java
@@ -1,14 +1,35 @@
 package festivalmanager.festival;
 
+import javax.validation.constraints.NotNull;
+
+import festivalmanager.ticket.Sort;
+
 public class FestivalIdForm {
 
+    @NotNull
     private final Long id;
+    @NotNull
+    private final Sort sort;
   
-    public FestivalIdForm(Long id) {
+    public FestivalIdForm(Long id, String sort_str) {
       this.id = id;
+      this.sort = stringToSort(sort_str);
+    }
+
+    public Sort stringToSort(String string){
+      if("CAMPINGTICKET".equals(string)){
+        return Sort.CAMPINGTICKET;
+      }
+      else{
+        return Sort.DAYTICKET;
+      }
     }
   
     public Long getId() {
-      return id;
+      return this.id;
+    }
+
+    public Sort getSort() {
+      return this.sort;
     }
   }
