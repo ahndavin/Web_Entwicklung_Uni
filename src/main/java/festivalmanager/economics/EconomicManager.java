88a8410a1edcebafae349a2dcecb0188430dad9b
Kurdey <tobias.neubauer@mailbox.tu-dresden.de>
2019-12-03 21:02:36 +0100

    Fully used the potential of Salespoint accountancy

--- src/main/java/festivalmanager/economics/EconomicManager.java
+++ src/main/java/festivalmanager/economics/EconomicManager.java
@@ -1,29 +1,58 @@
 package festivalmanager.economics;
 
-import java.util.ArrayList;
+import javax.money.MonetaryAmount;
 
 import org.javamoney.moneta.Money;
-//import org.salespointframework.accountancy.Accountancy;
+import org.salespointframework.accountancy.Accountancy;
+import org.salespointframework.accountancy.AccountancyEntry;
+import org.springframework.data.util.Streamable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional
 public class EconomicManager{
-    private static ArrayList<EconomicEntry> allEntrys;
 
-    public EconomicManager(){
-        allEntrys = new ArrayList<EconomicEntry>();
+    private final Accountancy accountency;
+
+    public EconomicManager(Accountancy accountency) {
+        this.accountency = accountency;
     }
 
-    public static void addEntry(javax.money.MonetaryAmount value, String description){
-        EconomicEntry ecoentry = new EconomicEntry(value, description);
-        allEntrys.add(ecoentry);
+	public void add(int amount, String description){
+        MonetaryAmount value = Money.of(amount, "EUR");
+        AccountancyEntry entry = new AccountancyEntry(value, description);
+        accountency.add(entry);
     }
 
-    public static void addEntry(int value, String description){
-        javax.money.MonetaryAmount amount = Money.of(value, "EUR");
-        EconomicEntry ecoentry = new EconomicEntry(amount, description);
-        allEntrys.add(ecoentry);
+    public Streamable<AccountancyEntry> getAll(){
+        return accountency.findAll();
     }
 
-    public ArrayList<EconomicEntry> getAll(){
-        return allEntrys;        
+    public MonetaryAmount getRevenues(){
+        MonetaryAmount sum = Money.of(0, "EUR");
+        for (AccountancyEntry entry : getAll()){
+            if(entry.isRevenue() == true){
+                sum = sum.add(entry.getValue());
+            }
+        }
+        return sum;
+    }
+
+    public MonetaryAmount getExpenses(){
+        MonetaryAmount sum = Money.of(0, "EUR");
+        for (AccountancyEntry entry : getAll()){
+            if(entry.isExpense() == true){
+                sum = sum.add(entry.getValue());
+            }
+        }
+        return sum;
+    }
+
+    public MonetaryAmount getSum(){
+        MonetaryAmount sum = Money.of(0, "EUR");
+        sum = sum.add(getRevenues());
+        sum = sum.subtract(getExpenses());
+        return sum;
     }
 }
