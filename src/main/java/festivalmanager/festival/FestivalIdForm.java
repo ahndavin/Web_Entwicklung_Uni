e8fba2c0f53601886b46939e1492afdcfd3b33af
Kurdey <tobias.neubauer@mailbox.tu-dresden.de>
2019-12-03 13:42:54 +0100

    added FestivalIdForm

---
+++ src/main/java/festivalmanager/festival/FestivalIdForm.java
@@ -0,0 +1,14 @@
+package festivalmanager.festival;
+
+public class FestivalIdForm {
+
+    private final Long id;
+  
+    public FestivalIdForm(Long id) {
+      this.id = id;
+    }
+  
+    public Long getId() {
+      return id;
+    }
+  }
