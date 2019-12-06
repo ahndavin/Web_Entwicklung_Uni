58ad605d4910ba1f8d5380cfe4dd95d727bb722c
jonashoen <hoenisch.jonas@gmail.com>
2019-12-06 19:31:43 +0100

    implemented function to pass the current inventory to the model

--- src/main/java/festivalmanager/inventory/InventoryController.java
+++ src/main/java/festivalmanager/inventory/InventoryController.java
@@ -1,4 +1,21 @@
 package festivalmanager.inventory;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class InventoryController {
+	private InventoryManager inventory;
+
+	public InventoryController(InventoryManager inventory) {
+		this.inventory = inventory;
+	}
+
+	@GetMapping("/inventory")
+	public String inventory(Model model) {
+		model.addAttribute("inventory", inventory.findAll());
+
+		return "inventory";
+	}
 }
