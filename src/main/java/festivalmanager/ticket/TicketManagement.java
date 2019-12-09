9e6dc5b88f981e5b2ca83b49f944983abe567afa
Kurdey <tobias.neubauer@mailbox.tu-dresden.de>
2019-12-09 15:11:39 +0100

    Split up lines to make Sonarcube happy

--- src/main/java/festivalmanager/ticket/TicketManagement.java
+++ src/main/java/festivalmanager/ticket/TicketManagement.java
@@ -17,7 +17,8 @@ public class TicketManagement{
     private final DayticketRepository dayticketRepository;
     private final CampingticketRepository campingticketRepository;
 
-    public TicketManagement(FestivalRepository festivalrepository, DayticketRepository dayticketRepository, CampingticketRepository campingticketRepository){
+    public TicketManagement(FestivalRepository festivalrepository, 
+    DayticketRepository dayticketRepository, CampingticketRepository campingticketRepository){
 		Assert.notNull(festivalrepository, "FestivalRepository must not be null!");
 		Assert.notNull(dayticketRepository, "TagesticketRepository must not be null!");
 		Assert.notNull(campingticketRepository, "CampingticketRepository must not be null!");
@@ -36,7 +37,8 @@ public class TicketManagement{
       }
 
       public boolean dayTicketIsAvailable(Sort sort, Festival festival){
-        if(festival.isSellingTickets() != false && sort == Sort.DAYTICKET && festival.getTicketBuilder().getAmountDaytickets().isGreaterThan(Quantity.NONE)){
+        if(festival.isSellingTickets() != false && sort == Sort.DAYTICKET && 
+        festival.getTicketBuilder().getAmountDaytickets().isGreaterThan(Quantity.NONE)){
             return true;
         }
         return false;
@@ -44,7 +46,8 @@ public class TicketManagement{
     }
 
     public boolean campingTicketIsAvailable(Sort sort, Festival festival){
-        if(festival.isSellingTickets() != false && sort == Sort.CAMPINGTICKET && festival.getTicketBuilder().getAmountCampingtickets().isGreaterThan(Quantity.NONE)){
+        if(festival.isSellingTickets() != false && sort == Sort.CAMPINGTICKET && 
+        festival.getTicketBuilder().getAmountCampingtickets().isGreaterThan(Quantity.NONE)){
             return true;
         }
         return false;
@@ -65,7 +68,7 @@ public class TicketManagement{
 		Quantity newQuantity = festival.getTicketBuilder().getAmountCampingtickets().subtract(Quantity.of(1));
 
         festival.getTicketBuilder().setAmountCampingtickets(newQuantity);
-        Campingticket ticket = new Campingticket(festival.getName(), festival.getTicketBuilder().getPriceCampingticket());
+        Campingticket ticket=new Campingticket(festival.getName(), festival.getTicketBuilder().getPriceCampingticket());
         campingticketRepository.save(ticket);
         return ticket;
     }
