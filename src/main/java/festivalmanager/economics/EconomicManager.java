099ea621d7b01225352338ce70b49e5bc571d4af
Kurdey <tobias.neubauer@mailbox.tu-dresden.de>
2019-12-13 13:46:22 +0100

    expanded functions

--- src/main/java/festivalmanager/economics/EconomicManager.java
+++ src/main/java/festivalmanager/economics/EconomicManager.java
@@ -2,6 +2,8 @@ package festivalmanager.economics;
 
 import javax.money.MonetaryAmount;
 
+import java.util.ArrayList;
+
 import org.javamoney.moneta.Money;
 import org.salespointframework.accountancy.Accountancy;
 import org.salespointframework.accountancy.AccountancyEntry;
@@ -9,29 +11,48 @@ import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import festivalmanager.festival.Festival;
+import festivalmanager.festival.FestivalRepository;
+
 @Service
 @Transactional
 public class EconomicManager{
 
     private final Accountancy accountency;
+    private final FestivalRepository festivalRepository;
 
-    public EconomicManager(Accountancy accountency) {
+    public EconomicManager(Accountancy accountency, FestivalRepository festivalRepository) {
         this.accountency = accountency;
+        this.festivalRepository = festivalRepository;
     }
 
-	public void add(int amount, String description){
+    public Festival findById(Long id) {
+        return festivalRepository.findById(id).isPresent() ? festivalRepository.findById(id).get() : null;
+      }
+
+	public void add(int amount, String description, Festival festival){
         MonetaryAmount value = Money.of(amount, "EUR");
         AccountancyEntry entry = new AccountancyEntry(value, description);
         accountency.add(entry);
+        festival.getEconomicList().add(entry);
     }
 
-    public Streamable<AccountancyEntry> getAll(){
-        return accountency.findAll();
+    public void add(Money value, String description, Festival festival){
+        System.out.println("Before adding the entry");
+        AccountancyEntry entry = new AccountancyEntry(value, description);
+        accountency.add(entry);
+        festival.getEconomicList().add(entry);
+        System.out.println("After adding the entry");
     }
 
-    public MonetaryAmount getRevenues(){
+    public ArrayList<AccountancyEntry> getAll(Festival festival){
+        return festival.getEconomicList();
+    }
+
+    public MonetaryAmount getRevenues(Festival festival){
+        ArrayList<AccountancyEntry> entrys = festival.getEconomicList();
         MonetaryAmount sum = Money.of(0, "EUR");
-        for (AccountancyEntry entry : getAll()){
+        for (AccountancyEntry entry : entrys){
             if(entry.isRevenue() == true){
                 sum = sum.add(entry.getValue());
             }
@@ -39,9 +60,10 @@ public class EconomicManager{
         return sum;
     }
 
-    public MonetaryAmount getExpenses(){
+    public MonetaryAmount getExpenses(Festival festival){
+        ArrayList<AccountancyEntry> entrys = festival.getEconomicList();
         MonetaryAmount sum = Money.of(0, "EUR");
-        for (AccountancyEntry entry : getAll()){
+        for (AccountancyEntry entry : entrys){
             if(entry.isExpense() == true){
                 sum = sum.add(entry.getValue());
             }
@@ -49,10 +71,10 @@ public class EconomicManager{
         return sum;
     }
 
-    public MonetaryAmount getSum(){
+    public MonetaryAmount getSum(Festival festival){
         MonetaryAmount sum = Money.of(0, "EUR");
-        sum = sum.add(getRevenues());
-        sum = sum.subtract(getExpenses());
+        sum = sum.add(getRevenues(festival));
+        sum = sum.subtract(getExpenses(festival));
         return sum;
     }
 }
