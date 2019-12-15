515110bc5a82565007b8a72ed7a4d6dbdb843206
Igor <cpaearn@gmx.de>
2019-12-16 00:34:15 +0100

    Rewrote the Contracts Structure to Implement Crud
    Had to rewrite Contract and how it is implemented to enable CRUD Operations.
    Frontend will follow soon but it will do the job for now.

--- src/main/java/festivalmanager/location/LocationDataInitializer.java
+++ src/main/java/festivalmanager/location/LocationDataInitializer.java
@@ -29,10 +29,10 @@ public class LocationDataInitializer implements DataInitializer {
 		
 		locationManager.findAll().get(0).addArea(new Area("B", 700, 4, Type.STAGE));
 		locationManager.findAll().get(0).getAllAreas().get(1).addStage(new Stage("first", "/img/location/lineup_poster/first.jpg"));
-		locationManager.findAll().get(0).getAllAreas().get(1).getAllStages().get(0).addLineup(new Lineup(new Date(2019, 12, 22, 18, 00), 
-				new Contract("500", "Max", true, "5", "50", "1000")));
-		locationManager.findAll().get(0).getAllAreas().get(1).getAllStages().get(0).addLineup(new Lineup(new Date(2019, 12, 22, 19, 00), 
-				new Contract("1000", "John", true, "10", "100", "2000")));
+		//locationManager.findAll().get(0).getAllAreas().get(1).getAllStages().get(0).addLineup(new Lineup(new Date(2019, 12, 22, 18, 00),
+		//		new Contract("500", "Max", true, "5", "50", "1000")));
+		//locationManager.findAll().get(0).getAllAreas().get(1).getAllStages().get(0).addLineup(new Lineup(new Date(2019, 12, 22, 19, 00),
+				//new Contract("1000", "John", true, "10", "100", "2000")));
 		
 		locationManager.findAll().get(0).getAllAreas().get(1).addStage(new Stage("second", "/img/location/lineup_poster/second.jpg"));
 		locationManager.findAll().get(0).getAllAreas().get(1).addStage(new Stage("third", "/img/location/lineup_poster/third.jpg"));
@@ -45,7 +45,7 @@ public class LocationDataInitializer implements DataInitializer {
 		locationManager.findAll().get(1).addArea(new Area("6-10", 500, 5, Type.STAGE));
 		locationManager.findAll().get(1).getAllAreas().get(0).addStage(new Stage("Erste", "/img/location/lineup_poster/Erste.jpg"));
 		locationManager.findAll().get(1).getAllAreas().get(0).addStage(new Stage("Zweite", "/img/location/lineup_poster/Zweite.jpg"));
-		locationManager.findAll().get(1).getAllAreas().get(0).getAllStages().get(0).addLineup(new Lineup(new Date(), 
-				new Contract("500", "John", true, "5", "50", "1000")));
+		//locationManager.findAll().get(1).getAllAreas().get(0).getAllStages().get(0).addLineup(new Lineup(new Date(),
+		//		new Contract("500", "John", true, "5", "50", "1000")));
 	}
 }
