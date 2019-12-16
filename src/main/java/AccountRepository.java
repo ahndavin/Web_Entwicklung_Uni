34838e16fd0026875c835e37298e564977f8eb72
Kitsunox <marcus@fiedler-jr.de>
2019-12-16 06:01:37 +0100

    removing old not used files

--- src/main/java/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.util.Streamable;
-
-interface AccountRepository extends CrudRepository<Account, Long> {
-
-	@Override
-	Streamable<Account> findAll();
-}
