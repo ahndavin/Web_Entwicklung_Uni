b420247e6667eed4a9175c434e0e756f72a2958a
Kitsunox <marcus@fiedler-jr.de>
2019-12-20 21:41:03 +0100

    added new querry methods to MessageRepository

--- src/main/java/festivalmanager/staff/MessageRepository.java
+++ src/main/java/festivalmanager/staff/MessageRepository.java
@@ -1,11 +1,19 @@
 package festivalmanager.staff;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.util.Streamable;
 
 import java.util.Collections;
 import java.util.List;
 
-public interface MessageRepository extends CrudRepository<Message, Long> {
+interface MessageRepository extends CrudRepository<Message, Long> {
+
+
+	Streamable<Message> findAllByReceiver(Account account);
+	Streamable<Message> findAllBySender(Account account);
+
+	@Override
+	Streamable<Message> findAll();
 
 
 }
