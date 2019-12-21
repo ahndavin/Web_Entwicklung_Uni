23fa7944be0e9ad6bc79e51cd750148954421556
Kitsunox <marcus@fiedler-jr.de>
2019-12-21 18:06:48 +0100

    added checking for messages and search options

--- src/main/java/festivalmanager/staff/MessageManagement.java
+++ src/main/java/festivalmanager/staff/MessageManagement.java
@@ -4,8 +4,10 @@ package festivalmanager.staff;
 import org.apache.juli.logging.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import javax.validation.constraints.Null;
 import java.util.ArrayList;
@@ -14,10 +16,14 @@ import java.util.List;
 
 @Service
 public class MessageManagement {
+
+	@Autowired
 	private final MessageRepository messages;
 	private static final Logger LOG = LoggerFactory.getLogger(MessageManagement.class);
 
-	public MessageManagement(MessageRepository messages){this.messages = messages;}
+	public MessageManagement(MessageRepository messages){
+		Assert.notNull(messages, "MessageRepository must not be null");
+		this.messages = messages;}
 
 
 
@@ -27,14 +33,19 @@ public class MessageManagement {
 	}
 
 	public boolean anyMessages(Account account){
-		try{
-
-			findAllByReceiver(account);
-			return true;
-
-		} catch (NullPointerException error){
+		Streamable<Message> allReceived = findAllByReceiver(account);
+		if(allReceived.isEmpty()){
 			return false;
 		}
+		return true;
+	}
+
+	public boolean anySentMessages(Account account){
+		Streamable<Message> allSent = findAllBySender(account);
+		if(allSent.isEmpty()){
+			return false;
+		}
+		return true;
 	}
 
 	public Streamable<Message> findAllBySender(Account sender) {
@@ -57,4 +68,9 @@ public class MessageManagement {
 
 		return messages.findAll();
 	}
+
+	public Streamable<Message> findByReceiver(Account receiver){return messages.findAllByReceiver(receiver);}
+
+	public Streamable<Message> findBySender(Account sender){return messages.findAllBySender(sender);}
+
 }
