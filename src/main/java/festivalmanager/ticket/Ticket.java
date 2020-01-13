92f1f113efeac262b9fe59c6d02945de34579984
Kurdey <tobias.neubauer@mailbox.tu-dresden.de>
2020-01-13 20:59:05 +0100

    tickets are festival specific

--- src/main/java/festivalmanager/ticket/Ticket.java
+++ src/main/java/festivalmanager/ticket/Ticket.java
@@ -3,6 +3,9 @@ package festivalmanager.ticket;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import festivalmanager.festival.Festival;
 
 @Entity
 public class Ticket{
@@ -12,10 +15,12 @@ public class Ticket{
     private javax.money.MonetaryAmount price;
     protected Sort sort;
     private boolean used = false;
+    @OneToOne private Festival festival;
 
-    public Ticket(String name, javax.money.MonetaryAmount price){
+    public Ticket(String name, javax.money.MonetaryAmount price, Festival festival){
         this.name = name;
         this.price = price;
+        this.festival = festival;
     }
 
     public String getName(){
@@ -36,6 +41,10 @@ public class Ticket{
 
     public boolean getUsed(){
         return this.used;
+    }
+
+    public Festival getFestival(){
+        return this.festival;
     }
 
     public void setUsed(boolean used){
